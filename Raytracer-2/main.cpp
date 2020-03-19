6aeb700d44d4a6c944e0b8c529f14d4b5e66a7a4
Pauline <pauline.lemeille@ecl16.ec-lyon.fr>
2020-03-19 11:01:03 +0100

    Initial Commit

---
+++ Raytracer-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raytracer-2
+//
+//  Created by Pauline Lemeille on 19/03/2020.
+//  Copyright © 2020 Pauline Lemeille. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
